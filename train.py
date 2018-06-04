dd85f1fe41f64e0577bfc3ad6cd669725b74fa4d
chikanamakalaka <mhocker@gmail.com>
2018-06-04 09:32:14 -0700

    updates to large brain

--- train.py
+++ train.py
@@ -6,6 +6,6 @@ from textgenrnn import textgenrnn
 
 textgen = textgenrnn()
 
-textgen.train_from_file('Psa.txt', new_model=False, num_epochs=5, gen_epochs=1, train_size=3, dropout=0.2)
+textgen.train_from_file('corpus/Psalms_test.txt', new_model=True, num_epochs=10, gen_epochs=1, train_size=3, dropout=0.2, rnn_layers=3,   rnn_size=256,rnn_bidirectional=True, max_length=100, max_words=10000, dim_embeddings=100, word_level=False, single_text=False, name='Psalms')
 textgen.generate()
 print(textgen.model.summary())
