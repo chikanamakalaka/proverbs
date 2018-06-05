0630f188d345ba7e7d18908389b02797669d92a6
Matthew Hocker <mhocker@gmail.com>
2018-06-04 17:06:29 -0700

    added book

--- train.py
+++ train.py
@@ -7,6 +7,6 @@ from textgenrnn import textgenrnn
 textgen = textgenrnn()
 
 # textgen.train_from_file('corpus/Psalms.txt', new_model=True, num_epochs=50, gen_epochs=10, train_size=2, dropout=0.2, rnn_layers=3, rnn_size=256, rnn_bidirectional=False, max_length=100, max_words=10000, dim_embeddings=100, word_level=False, single_text=False, name='Psalms')
-textgen.train_from_file('corpus/colors.txt', new_model=True, num_epochs=50, gen_epochs=5, word_level=False, name='Colors', rnn_bidirectional=True)
+textgen.train_from_file('corpus/0200601.txt', new_model=True, num_epochs=50, gen_epochs=5, name='Mein_Kampf')
 
 print(textgen.model.summary())
