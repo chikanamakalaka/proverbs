0d5767a064a0460967e3d8b8d2dce0e24a3834f2
Matthew Hocker <mhocker@gmail.com>
2018-06-05 17:40:28 -0700

    changes

--- gen_line.py
+++ gen_line.py
@@ -3,5 +3,5 @@ import warnings
 warnings.filterwarnings("ignore")
 from textgenrnn import textgenrnn
 
-textgen = textgenrnn('Colors_weights.hdf5')
-textgen.generate(10, temperature=2.5)
+textgen = textgenrnn('Proverb_weights.hdf5')
+textgen.generate(10, temperature=0.5)
--- train.py
+++ train.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 import warnings
 warnings.filterwarnings("ignore")
-
 from textgenrnn import textgenrnn
 
 textgen = textgenrnn()
 
-# textgen.train_from_file('corpus/Psalms.txt', new_model=True, num_epochs=50, gen_epochs=10, train_size=2, dropout=0.2, rnn_layers=3, rnn_size=256, rnn_bidirectional=False, max_length=100, max_words=10000, dim_embeddings=100, word_level=False, single_text=False, name='Psalms')
-textgen.train_from_file('corpus/0200601.txt', new_model=True, num_epochs=50, gen_epochs=5, name='Mein_Kampf')
-
+textgen.train_from_file('corpus/Psalms.txt', new_model=True, num_epochs=50, gen_epochs=10, train_size=2, rnn_layers=3, rnn_size=256, rnn_bidirectional=False, max_length=100, max_words=10000, dim_embeddings=100, word_level=False, single_text=False, name='Proverbs')
+# textgen.train_from_file('corpus/Proverbs.txt', num_epochs=30, gen_epochs=1, name='Proverbs', dropout=0.5, single_text=False)
 print(textgen.model.summary())
